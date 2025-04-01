ecfd0d03e3b76a379212c2821cfc7fde4efb6729
John Arocha <john.arocha@clover.com>
2025-04-01 11:24:51 -0600

    added encryption functions

---
+++ src/main/java/org/snippet/Security/AesEncryptionUtil.java
@@ -0,0 +1,40 @@
+package org.snippet.Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AesEncryptionUtil {
+
+    // Generate a new AES key
+    public static String generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256); // AES-256 (ensure your JDK supports it)
+        SecretKey secretKey = keyGen.generateKey();
+        return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+    }
+
+    // Encrypt plaintext using AES
+    public static String encrypt(String plaintext, String base64Key) throws Exception {
+        byte[] decodedKey = Base64.getDecoder().decode(base64Key);
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    // Decrypt ciphertext using AES
+    public static String decrypt(String encryptedText, String base64Key) throws Exception {
+        byte[] decodedKey = Base64.getDecoder().decode(base64Key);
+        SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decryptedBytes);
+    }
+}
