ea35bfc304bc94109709fb10ebc95d1931f78b69
John Arocha <john.arocha@clover.com>
2025-04-01 08:04:14 -0600

    created user model

--- src/main/java/org/snippet/Controller/SnippetController.java
+++ src/main/java/org/snippet/Controller/SnippetController.java
@@ -75,6 +75,5 @@ public class SnippetController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
     }
 }
---
+++ src/main/java/org/snippet/Modal/User.java
@@ -0,0 +1,42 @@
+package org.snippet.Modal;
+
+import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
