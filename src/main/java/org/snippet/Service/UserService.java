c58cc67887e9a8a24805b1c8735fef256cb9c4f7
John Arocha <john.arocha@clover.com>
2025-04-01 08:14:25 -0600

    added user service

--- src/main/java/org/snippet/Service/UserService.java
+++ src/main/java/org/snippet/Service/UserService.java
@@ -6,5 +6,14 @@ import java.util.Optional;
 
 public interface UserService {
     // Save
-    User saveUser();
+    User saveUser(User user);
+
+    // Update
+    User updateUser(User user);
+
+    // Delete
+    void deleteUser(User user);
+
+    // Find by
+    Optional<User> findById(Integer id);
 }
